a4a5feff2876c92dbfbfffc878eefe9776055ea1
Jarred Sumner <jarred@jarredsumner.com>
2012-05-27 13:16:13 -0700

    Fixed XOAuth support -- can now deliver and search when authenticated with XOAuth.

--- lib/gmail/client/xoauth.rb
+++ lib/gmail/client/xoauth.rb
@@ -5,15 +5,18 @@ module Gmail
     class XOAuth < Base
       attr_reader :token
       attr_reader :secret
+      attr_reader :token_secret
       attr_reader :consumer_key
       attr_reader :consumer_secret
 
       def initialize(username, options={})
+        @username        = username
         @token           = options.delete(:token)
         @secret          = options.delete(:secret)
+        @token_secret    = @secret
         @consumer_key    = options.delete(:consumer_key)
         @consumer_secret = options.delete(:consumer_secret)
-       
+        
         super(username, options)
       end
 
